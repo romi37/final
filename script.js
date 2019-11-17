3aa6ca138b4402c1a5ae047603ba481aa4069489
romifrexter@yahoo.co.id <romifrexter@yahoo.co.id>
2019-11-17 13:54:50 +0700

    dinamis

--- script.js
+++ script.js
@@ -2,11 +2,12 @@
 const carousel = document.getElementById("carousel");
 const next = document.getElementById("next");
 const prev = document.getElementById("prev");
-
+const slider = document.getElementsByClassName("slider")[0];
 const carouselItem = carousel.children;
 
 var width = 0;
 var slide = false;
+var index = 0;
 
 next.addEventListener("click", function() {
   slide = true;
@@ -14,8 +15,11 @@ next.addEventListener("click", function() {
     const item = carouselItem[i];
 
     if (slide) {
-      width -= item.clientWidth;
-      slide = false;
+      if (index < 2){
+        width -= item.clientWidth;
+        slide = false;
+        index++;
+      }
     }
 
     item.style.transform = `translateX(${width}px)`;
@@ -29,8 +33,11 @@ prev.addEventListener("click", function() {
     const item = carouselItem[i];
 
     if (slide) {
+      if (index > 0){
       width += item.clientWidth;
       slide = false;
+      index--;
+      }
     }
 
     item.style.transform = `translateX(${width}px)`;
@@ -46,13 +53,15 @@ fetch('https://jsonplaceholder.typicode.com/todos')
   .then(json => {
     json.map(item =>{
         listContainer.innerHTML += `
-        <article>
-        <div class="article-image">image</div>
-        <div class="article-text">
-            <h1>${item.title}</h1>
-            <p>${item.title}</p>
-            <button class="button-prymary">Baca Selengkapnya...</button>
-        </div>
-    </article>`
+        <div class="card">
+  <div class="card-header">
+    Featured
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">${item.title}</h5>
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>`
     })
   })
